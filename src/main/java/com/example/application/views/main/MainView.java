9ff16f0f05903e9c00155227f0cb6d5a741ba21d
FleetyCodes <laszlo.murzsicz94@gmail.com>
2024-01-17 12:18:23 +0100

    init vaadin project

---
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -0,0 +1,12 @@
+package com.example.application.views.main;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+
+    }
+}
